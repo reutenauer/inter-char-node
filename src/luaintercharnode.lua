ad69ca61529aa8d2ca32af7fa21075a825ba70be
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2012-08-02 23:59:51 +0100

    First experiment looks good.

--- src/luaintercharnode.lua
+++ src/luaintercharnode.lua
@@ -1,11 +1,29 @@
 function intercharnode(head)
-  local glyph_id = node.id('glyph')
+  local glyph_id, kern_id = node.id('glyph'), node.id('kern')
   local node_types = node.types
 
-  for n in node.traverse(head) do
-    local node_info = 'NODE TYPE ' .. node_types()[n.id]
-    if n.id == glyph_id then node_info = node_info .. ' ' .. n.char end
+  local function isascii(charnode)
+    local c = charnode.char
+    return c > 0x40 and c <= 0x5A or c > 0x60 and c <= 0x7A
+  end
+
+  local emspace = node.new(kern_id, 1)
+  emspace.kern = tex.sp('1em')
+
+  local prevnode
+  for currnode in node.traverse(head) do
+    local node_info = 'NODE TYPE ' .. node_types()[currnode.id]
+    if currnode.id == glyph_id then node_info = node_info .. ' ' .. currnode.char end
+    if prevnode and prevnode.id == glyph_id and currnode.id == glyph_id then
+      texio.write_nl(prevnode.char .. ' and ' .. currnode.char)
+      if isascii(prevnode) and isascii(currnode) then
+        texio.write_nl("Inserting stuff")
+        node.insert_after(head, prevnode, node.copy(emspace))
+      end
+    end
+
     texio.write_nl('term and log', node_info)
+    prevnode = currnode
   end
 
   return head
