c8384512af7bf43815f8dfbf694fb85ca1ae0782
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2012-08-02 23:33:00 +0100

    Getting reacquainted with node list processing.

--- src/luaintercharnode.lua
+++ src/luaintercharnode.lua
@@ -1,3 +1,16 @@
+function intercharnode(head)
+  local glyph_id = node.id('glyph')
+  local node_types = node.types
+
+  for n in node.traverse(head) do
+    local node_info = 'NODE TYPE ' .. node_types()[n.id]
+    if n.id == glyph_id then node_info = node_info .. ' ' .. n.char end
+    texio.write_nl('term and log', node_info)
+  end
+
+  return head
+end
+
 function activate()
   luatexbase.add_to_callback('pre_linebreak_filter', intercharnode, 'Inter-char node processing')
 end
